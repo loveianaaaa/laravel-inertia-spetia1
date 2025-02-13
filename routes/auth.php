fd234a280b16ba40bc76282dacb8bc73e6f77381
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-13 11:52:31 +0700

    menambahkan komentar

--- routes/auth.php
+++ routes/auth.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
-        ->name('register');
+        ->name('register'); 
+        //adalah route di Laravel yang menangani halaman pendaftaran pengguna (register page)
 
     Route::post('register', [RegisteredUserController::class, 'store']);
 
--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,6 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('inspire', function () {// Mendefinisikan perintah custom bernama inspire yang bisa dijalankan melalui terminal 
+    $this->comment(Inspiring::quote());//Berasal dari Laravel, fungsi ini akan mengembalikan kutipan inspiratif acak.
+})->purpose('Display an inspiring quote')->hourly();//Ini adalah metode yang memberikan deskripsi singkat tentang perintah.
