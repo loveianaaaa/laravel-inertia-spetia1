0de84b2f5dbcf7268a50d66302452bec2f83a594
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-13 11:09:03 +0700

    menambahkan komentar

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -5,6 +5,7 @@ use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
+//new class extends Migration → Membuat anonymous class yang mewarisi (extends) kelas Migration dari Laravel.                                                Migration → Kelas dasar yang digunakan dalam Laravel untuk mengelola migrasi database.
 {
     /**
      * Run the migrations.
@@ -19,6 +20,13 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            //adalah shorthand untuk menambahkan kolom id sebagai primary key.Secara default, Laravel akan membuat kolom
+            // adalah bagian dari Laravel Migration yang digunakan untuk membuat kolom name dengan tipe data VARCHAR
+            //$table->string('email'); → Membuat kolom email dengan tipe VARCHAR(255) secara default.                                                    ->unique(); → Menjadikan kolom email sebagai unique key, sehingga tidak boleh ada dua email yang sama dalam tabel.
+            //$table->timestamp('email_verified_at'); → Membuat kolom dengan tipe data TIMESTAMP, yang menyimpan tanggal dan waktu.                        ->nullable(); → Memungkinkan nilai NULL, yang berarti email belum diverifikasi.
+            //$table->string('password'); → Membuat kolom password dengan tipe data VARCHAR(255) (default untuk string di Laravel).
+            // $table->rememberToken(); Membuat kolom remember_token dengan tipe data VARCHAR(100) secara default.
+            //$table->timestamps(); secara otomatis menambahkan
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
