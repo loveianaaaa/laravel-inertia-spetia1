c11e286457bacbfc7b49c1bea93abaa449da8dfa
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-13 11:26:50 +0700

    menambahkan komentar

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -16,22 +16,27 @@ class UserTableSeeder extends Seeder
     public function run(): void
     {
         //create user
-        $user = User::create([
-            'name'      => 'Syahrizaldev',
-            'email'     => '2223602.love@smkn-2sbg.sch.id',
-            'password'  => bcrypt('2223602'),
+        $user = User::create([ //Metode Eloquent ORM untuk menambahkan data ke tabel users.
+            'name'      => 'Syahrizaldev', //menambahkan nama pengguna
+            'email'     => '2223602.love@smkn-2sbg.sch.id', //Menentukan email pengguna, yang biasanya bersifat unik dalam sistem.
+            'password'  => bcrypt('2223602'),//Mengenkripsi kata sandi menggunakan bcrypt() sebelum disimpan di database.
         ]);
 
         //get all permissions
         $permissions = Permission::all();
+        //Mengambil semua data dari tabel permissions dalam bentuk koleksi Laravel (Collection).
+        //$permissions Berisi daftar semua izin yang telah dibuat dalam sistem menggunakan Spatie Laravel Permission.
 
         //get role admin
         $role = Role::find(1);
+        //Role::find(1)Mencari role berdasarkan ID 1 dalam tabel roles.Jika role ditemukan, objek Role akan dikembalikan.                                    Jika role tidak ditemukan, nilai yang dikembalikan adalah null.                                                                                      $role Variabel yang menyimpan hasil pencarian role.
 
         //assign permission to role
         $role->syncPermissions($permissions);
+        //Menghapus semua izin yang sebelumnya dimiliki oleh role.                                                                                     Menambahkan daftar izin baru yang diberikan dalam $permissions.
 
         //assign role to user
         $user->assignRole($role);
+        //Memberikan satu atau lebih peran (role) ke user.
     }
 }
